21934df93b47e5590b620016b093668b7dcf2dd9
Dion Fananie <dionfananie@gmail.com>
2024-09-08 13:57:29 +0700

    feat(init): docker config

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ const PORT = 3030;
 app.get("/", (req: Request, res: Response) => {
   res.send("hello from server");
 });
+app.get("/api/items", (req: Request, res: Response) => {
+  res.send("hello from server items");
+});
 app.listen(PORT, () => {
   console.log(`server running ins ${PORT}`);
 });
